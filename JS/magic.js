a579228585cca0f761ad63f3eafa4e05776fd995
tilltheant <58863820+tilltheant@users.noreply.github.com>
2020-01-14 20:06:33 +0100

    ad

---
+++ JS/magic.js
@@ -0,0 +1,11 @@
+var zoekbutton = document.querySelector('p');
+var zoekveld = document.querySelector('svg');
+
+function show () {
+	zoekveld.classList.toggle('yes');
+}
+
+zoekbutton.addEventListener('click', show);
+
+
+//https://dlo.mijnhva.nl/d2l/le/content/32112/viewContent/218497/View//
